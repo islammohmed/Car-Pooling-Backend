de524841d9cc4faa048219bac49293c7e4eea0ce
doniaabdelmoneim <donia.abdelmoneim19@gmail.com>
2025-06-03 00:12:45 +0300

    add car , tripParticipants

---
+++ Car-Pooling/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Pooling.Models
+{
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        [Required, StringLength (50)]
+        public string Model { get; set; }
+        [Required, StringLength (20)]
+        public string Color { get; set; }
+        [Required]
+        public int DriverId { get; set; }
+        [ForeignKey (nameof (DriverId))]
+        public User Driver { get; set; }
+
+        [Required, StringLength (20)]
+        public string PlateNumber { get; set; }
+    }
+}
---
+++ Car-Pooling/Models/TripParticipant.cs
@@ -0,0 +1,26 @@
+﻿using Car_Pooling.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Pooling.Models
+{
+    public class TripParticipant
+    {
+       [Key]
+        public int TripParticipantId { get; set; }
+        public int TripId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public JoinStatus Status { get; set; } 
+        public DateTime JoinedAt { get; set; }
+        [ForeignKey (nameof (TripId))]
+        public Trip Trip { get; set; }
+        [ForeignKey (nameof (UserId))]
+        public User User { get; set; }
+        [Required, Range (1, 10)]
+        public int SeatCount { get; set; }
+
+
+    }
+}
